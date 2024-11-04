8b383549938319421557408c8f6e6543b9470c54
gitboosting <chanfelix@gmail.com>
2024-11-04 13:19:50 -0600

    put source code; not working yet

--- src/imuf.cpp
+++ src/imuf.cpp
@@ -1,76 +1,119 @@
-// [[Rcpp::depends(BH)]]
+#include <RcppEigen.h>
 #include <Rcpp.h>
-#include <boost/math/constants/constants.hpp>
-#include <boost/qvm/all.hpp>
+
+// [[Rcpp::depends(RcppEigen)]]
+
 using namespace Rcpp;
-using namespace boost::math::constants;
-using namespace boost::qvm;
+using Eigen::Quaternion;
+using Eigen::QuaternionBase;
+using Eigen::VectorXd;
 
-vec<double, 3> numericVecToVec(NumericVector v) {
-  return vec<double, 3> { v[0], v[1], v[2] };
+VectorXd numericVecToVec(NumericVector v) {
+  VectorXd vout(3); vout << v[0], v[1], v[2];
+  return vout;
 }
 
-quat<double> vecToQuat(vec<double, 3> v) {
-  return quat<double> { 0, X(v), Y(v), Z(v) };
+NumericVector vecToNumericVec(VectorXd v) {
+  NumericVector vout = { v[0], v[1], v[2] };
+  return vout;
 }
 
-NumericVector quatToNumericVec(quat<double> q) {
-  return NumericVector { S(q), X(q), Y(q), Z(q) };
+// Eigen::Vector3d vxdTov3d(VectorXd v) {
+//   Eigen::Vector3d vout(v[0], v[1], v[2]);
+//   return vout;
+// }
+
+// [[Rcpp::export]]
+NumericVector nv2vec2nv(NumericVector vin) {
+  VectorXd v = numericVecToVec(vin);
+  NumericVector nv = vecToNumericVec(v);
+  return nv;
 }
 
-quat<double> numericVecToQuat(NumericVector v) {
-  return quat<double> { v[0], v[1], v[2], v[3] };
+Quaternion<double> vecToQuat(VectorXd v) {
+  return Quaternion<double> { 0, v[0], v[1], v[2] };
 }
 
-//'
-//' 'compUpdate' update orientation with 3-axis acc and gyr data
-//'
-//' @param acc A numeric 3-vector of 3-axis accelerometer readings in g
-//' @param gyr A numeric 3-vector of 3-axis gyroscope readings in rad/sec
-//' @param dt A numeric of time duration in sec
-//' @param initQuat A numeric 4-vector of the starting orientation in quaternion
-//' @param gain A numeric gain factor between 0 and 1
-//' @returns A numeric 4-vector of the ending orientation in quaternion
-//' @export
-//'
-//' @examples
-//' compUpdate(c(0, 0, -1), c(1, 0, 0), 0.1, c(1, 0, 0, 0), 0.1)
-//'
+Quaternion<double> numericVecToQuat(NumericVector v) {
+  Quaternion<double> q(v[0], v[1], v[2], v[3]);
+  return q;
+}
+
+NumericVector quatToNumericVec(Quaternion<double> q) {
+  return NumericVector { q.w(), q.x(), q.y(), q.z() };
+}
+
+Quaternion<double> qTimesD(Quaternion<double> q, double d) {
+  // a function to multiply a quaternion by a double
+  Quaternion<double> qout(q.coeffs() * d);
+  return qout;
+}
+
+Quaternion<double> operator* (const Quaternion<double>& q, double d) { return qTimesD(q, d); }
+Quaternion<double> operator* (double d, const Quaternion<double>& q) { return qTimesD(q, d); }
+Quaternion<double> operator/ (const Quaternion<double>& q, double d) { return qTimesD(q, 1/d); }
+
+Quaternion<double> operator+ (const Quaternion<double>& q1, const Quaternion<double>& q2) {
+  Quaternion<double> qout;
+  qout.w() = q1.w() + q2.w();
+  qout.x() = q1.x() + q2.x();
+  qout.y() = q1.y() + q2.y();
+  qout.z() = q1.z() + q2.z();
+  return qout;
+}
+
+// [[Rcpp::export]]
+NumericVector nv2quat2nv(NumericVector nvin) {
+  Quaternion<double> q = numericVecToQuat(nvin);
+  NumericVector nv = quatToNumericVec(q);
+  return nv;
+}
+
 // [[Rcpp::export]]
 NumericVector compUpdate(NumericVector acc, NumericVector gyr, double dt, NumericVector initQuat, double gain) {
-  //
-  // coordinate system is NED
-  const vec<double, 3> accVec = numericVecToVec(acc);
-  const vec<double, 3> gyrVec = numericVecToVec(gyr);
-  const quat<double> qt = numericVecToQuat(initQuat);
+
+  const VectorXd accVec = numericVecToVec(acc);
+  const VectorXd gyrVec = numericVecToVec(gyr);
+  const Quaternion<double> qt = numericVecToQuat(initQuat);
   //
   // make gryVec into a quat for Euler Equation
-  const quat<double> gyrQuat = vecToQuat(gyrVec);
-  const double gyrQuatNorm = mag(gyrQuat);
+  const Quaternion<double> gyrQuat = vecToQuat(gyrVec);
+  const double gyrQuatNorm = gyrQuat.norm();
   const double delta = gyrQuatNorm * dt / 2.0;
-  quat<double> qt1;
-  if (delta == 0) {
-    qt1 = qt;
-  } else {
-    qt1 = qt * cos(delta) + qt * 1 / gyrQuatNorm * sin(delta) * gyrQuat;
-  }
-  normalize(qt1);
+  Quaternion<double> qt1 = qt * cos(delta) + qt * (gyrQuat * 1 / gyrQuatNorm * sin(delta));
+  qt1.normalize();
   //
-  // rotate acc by qt1
-  const vec<double, 3> accWorld = qt1 * accVec;
+  // transform acc from body-fixed frame to world frame
+  const VectorXd accWorld = qt1 * accVec;
   //
   // complement with accelerometer info
-  const vec<double, 3> g = { 0, 0, -1 };
-  const vec<double, 3> n = cross(accWorld, g);
-  const double magN = mag(n);
-  const double qaDotG = dot(accWorld, g);
-  const double phi = std::atan2(magN, qaDotG);
+  VectorXd g(3); g << 0, 0, -1;
+  Quaternion<double> qAdj = Quaternion<double>::FromTwoVectors(accWorld, g);
   //
   // adjust rotation quaternion by nudging it towards g by gain * phi
-  quat<double> qAdj = rot_quat(n, gain * phi);
-  normalize(qAdj);
-  quat<double> qout = qAdj * qt1;
-  normalize(qout);
-
+  Quaternion<double> qout = qt1.slerp(gain, qAdj * qt1);
+  //
   return quatToNumericVec(qout);
 }
+
+/*** R
+# use realistic imu readings
+
+(vout <- nv2vec2nv(c(7, 8, 9)))
+#
+(nvout <- nv2quat2nv(c(5, 6, 7, 8)))
+#
+# use realistic imu readings
+#
+initQ <- c(0.9838937550736144, -0.008503796943710444, -0.0026035201363168387, -0.17853287049611438)
+acc <- c(-0.005615234, 0.019042969, -1.004150391)
+gyr <- c(0, -0.009587379919921934, -0.00745685105658519)
+dt <- 0.024
+gain <- 0.1
+qout <- compUpdate(acc, gyr, dt, initQ, gain)
+#
+qout  # should be 0.9838771143246333 -0.008654456431112963 -0.0025514591206403673 -0.17861806837067223
+#
+*/
+eigenExpt.cpp
+Displaying eigenExpt.cpp.
